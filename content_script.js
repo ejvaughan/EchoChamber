8a7613b6acb98a347b9011d2be094cff36fd2393
Ethan Vaughan <ejvaughan@gmail.com>
2017-02-21 19:08:34 -0600

    Refactor the header-finding code into a separate function

--- content_script.js
+++ content_script.js
@@ -1,4 +1,9 @@
-// Make a request to the boilerpipe web API, which can give us the plain text of the article w/o any markup
+// Makes a request to the boilerpipe (https://boilerpipe-web.appspot.com/) API, which gives us the plain text of the article w/o any markup
+//
+// callback takes two parameters:
+//  - success: A boolean value indicating whether the request was successful
+//  - json: A json object containing the API's response
+//
 function requestArticleText(callback) {
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function() {
@@ -15,39 +20,47 @@ function requestArticleText(callback) {
 	request.send(null);
 }
 
-function commonPrefixLength(str1, str2) {
-	var minLength = Math.min(str1.length, str2.length);
+// Returns the h1 element with the longest common prefix to text
+function headerElementMatching(text) {
 
-	for (var i = 0; i < minLength; ++i) {
-		if (str1[i] != str2[i]) {
-			return i;
+	function commonPrefixLength(str1, str2) {
+		var minLength = Math.min(str1.length, str2.length);
+
+		for (var i = 0; i < minLength; ++i) {
+			if (str1[i] != str2[i]) {
+				return i;
+			}
+		}
+
+		return minLength;
+	}
+
+	var headerElements = document.getElementsByTagName("h1");
+	var currentBestHeader = null;
+	var currentBestLength = 0;
+	for (var i = 0; i < headerElements.length; ++i) {
+		var header = headerElements[i];
+		var commonPrefixLen = commonPrefixLength(header.textContent.trim(), text);
+
+		if (commonPrefixLen > currentBestLength) {
+			currentBestLength = commonPrefixLen;
+			currentBestHeader = header;
 		}
 	}
 
-	return minLength;
+	return currentBestHeader;
 }
 
+
 console.log("content script document.URL = " + document.URL);
+
 requestArticleText(function(success, json) {
 	if (success) {
 		console.log("Retrieved article text: " + json.content);
 
-		// Find the h1 element that contains the article's title, and append the article's word count
-		var headerElements = document.getElementsByTagName("h1");
-		var currentBestHeader;
-		var currentBestLength = 0;
-		for (var i = 0; i < headerElements.length; ++i) {
-			var header = headerElements[i];
-			var commonPrefixLen = commonPrefixLength(header.textContent.trim(), json.title);
-
-			if (commonPrefixLen > currentBestLength) {
-				currentBestLength = commonPrefixLen;
-				currentBestHeader = header;
-			}
-		}
-
-		if (currentBestLength > 0) {
-			currentBestHeader.textContent = currentBestHeader.textContent + " (" + json.content.trim().split(/\s+/).length + " words)";
+		var header = headerElementMatching(json.title);
+		if (header) {
+			header.textContent = header.textContent + " (" + json.content.trim().split(/\s+/).length + " words)";
 		}
 	} else {
 		console.log("There was an error fetching the article text");
