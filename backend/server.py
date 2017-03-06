8ea80395ec46cdc7d414645a47e7fa4bf78a8dd0
Allen Hao <allen.hao@wustl.edu>
2017-03-05 20:37:07 -0600

    server updates

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,10 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from flask.json import jsonify
-import csv
 from newspaper import Article
-from sklearn.externals import joblib
+import pickle
 import timeit
-# import ssl
-# context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-# context.load_cert_chain('host.cert', 'host.key')
+
 
 
 app = Flask(__name__)
@@ -21,14 +18,16 @@ def score():
     a.download()
     a.parse()
     text = a.text
-    clf = joblib.load('model.p')
+    clf = pickle.load(open('/home/allen/flaskapp/model.p', 'rb'))
     pred = clf.predict([text])[0].item() # Return singular result
     score =  "Liberal" if pred else "Conservative"
     title = a.title
     time = timeit.default_timer() - start
-
     return jsonify({'score': score, 'title' : title, 'time':time})
+
+@app.route('/')
+def helloworld():
+    return "Website still under development"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-    # app.run(host='127.0.0.1',port='5000',  ssl_context=context)
