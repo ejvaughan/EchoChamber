0bf741fe9c5c90e00b9d79800955a69bb63618f7
Ethan Vaughan <ejvaughan@gmail.com>
2017-03-05 19:22:44 -0600

    Updated frontend to display the score in a popup instead of the web page's DOM. Also updated to work with the new backend.

---
+++ frontend/popup.js
@@ -0,0 +1,22 @@
+function getScore() {
+    chrome.tabs.query({active: true}, function(tabs) {
+        var url = tabs[0].url;
+        console.log("Getting score for url: " + url);
+
+        var serviceUrl = "https://allenhao.me/article";
+        var data = {"article": url};
+
+        jQuery.post(serviceUrl, data, function(res) {
+            res = JSON.parse(res);
+            var score = res.score;
+            console.log("Got score: " + score);
+
+            $("#score").text(score);
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Getting score...");
+    getScore();
+});
