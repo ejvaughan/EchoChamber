64173258483a31abf665f7d81bd1f1729fd14b02
Andrew Cukierwar <acukierwar@wustl.edu>
2017-03-07 12:04:55 -0600

    Created uniform scraper

--- data/urlScraper.py
+++ data/urlScraper.py
@@ -2,13 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import sys
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import *
 import string
-from collections import Counter
 
-def preprocess(url):
+def scrape(url):
+	# headers = {
+	#     'x-api-key': 'K0liTGBXgDZWwbnzDeZvzPcQhdFmeTLSTm4HVq50',
+	#     'Content-Type': 'application/json'
+	# }
+	# start = time.time()
+	# r = requests.get('https://mercury.postlight.com/parser?url=http://www.salon.com/2017/02/22/president-trumps-plan-to-round-up-the-undocumented-will-be-wonderful-for-private-prisons/', headers=headers)
+	# arr = r.content
+	# print arr
+
 	# convert to Mercury URL
 	mercury_base = "https://mercury.postlight.com/amp?url="
 	mercury_url = mercury_base + url
@@ -19,34 +24,23 @@ def preprocess(url):
 	request = requests.get(mercury_url)
 	html = request.text
 	soup = BeautifulSoup(html, 'html.parser')
-	date = soup.find('time', class_='hg-pubdate').text # extract date, not necessary for now
+	title = soup.find('h1', class_='hg-title').text # extract title
+	date = soup.find('time', class_='hg-pubdate').text # extract date
 	content = soup.find('div', class_='hg-article-body').find_all('p')
 	for tag in content:
 		parents = tag.findParents('footer')
 		if len(parents) == 0: # if footer is not a parent tag
-			text = text + "\n" + tag.text
-	print text
+			text = text + " " + tag.text
 
 	text = text.encode("utf-8")
 	text = text.replace("\xe2\x80\x99", "\'") # keep single quotes FIX POSSIBLE BUG
 	text = text.decode("utf-8")
 	text = text.encode("ascii", "ignore") # remove all non-ascii characters
 
-	text = text.lower() # make all text lowercase
-	text = text.translate(None, string.punctuation) # remove punctuation
-
-	tokens = nltk.word_tokenize(text) # convert text to tokens
-	tokens = [word for word in tokens if word not in stopwords.words('english')] # remove stop words
-
-	#stem tokens
-	stemmer = PorterStemmer()
-	stemmed = []
-	for item in tokens:
-		stemmed.append(str(stemmer.stem(item)))
-	tokens = stemmed
-
-	count = Counter(tokens)
-	print count.most_common(50)
+	toReturn = {"date":str(date), "title":str(title), "text":text, "url":url}
+	print toReturn
+	return toReturn
 
 url = "http://www.cnn.com/2017/03/05/politics/trump-russia-fallout/index.html"
-preprocess(url)
+# url = "http://www.breitbart.com/big-government/2017/02/24/donald-trump-thrills-conservatives-cpac/"
+scrape(url)
