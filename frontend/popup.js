3849611cc3075fa9f2f656208d8a81d179988461
Allen Hao <allen.hao@wustl.edu>
2017-03-06 20:32:58 -0600

    Stores local results and gives warning

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,16 +1,53 @@
+function toPercent(prop) {
+    return Math.round(prop * 100, 3);
+}
+
+function checkHistory(countThreshold, propThreshold) {
+    console.log("here")
+    chrome.storage.sync.get(["scoreTuple"], function(obj){
+        var tuple = obj["scoreTuple"];
+        var libProp = tuple[1] / tuple[0];
+        var conProp = 1 - libProp;
+        console.log(tuple[0])
+        console.log(tuple[0] > countThreshold)
+        console.log(libProp)
+        console.log(libProp > propThreshold)
+        if(tuple[0] > countThreshold && libProp > propThreshold) {
+            $("#reminder").text(toPercent(libProp) + "% of your news has been liberal. Consider checking out the other side!");
+        } else if(tuple[0] > countThreshold && conProp > propThreshold) {
+            $("#reminder").text(toPercent(conProp) + "% of your news has been conservative. Consider checking out the other side!");
+        }
+    });
+}
+function updateHistory(newscore){
+    // If new score is liberal, then increment value
+    var value = newscore == "Liberal" ? 1 : 0;
+    chrome.storage.sync.get(["scoreTuple"], function(obj){
+        // I can change this tuple to an object if that's better
+        var tuple = obj['scoreTuple']
+        console.log(tuple)
+        tuple[0] = (tuple[0] === undefined) ? 0 : tuple[0]; // Set to 0 if no prior value
+        tuple[1] = (tuple[1] === undefined) ? 0 : tuple[1];
+        var articleCount = tuple[0] +1;
+        var liberalCount = tuple[1] + value;
+        console.log(articleCount)
+        var newTuple = [articleCount, liberalCount];
+        chrome.storage.sync.set({"scoreTuple": newTuple});
+    });
+}
+
 function getScore() {
     chrome.tabs.getSelected(null, function(tab) {
         var url = tab.url;
         console.log("Getting score for url: " + url);
         var serviceUrl = "https://allenhao.me/article";
         var data = {"article": url};
-
         jQuery.post(serviceUrl, data, function(res) {
             $("#loading").hide();
             res = JSON.parse(res);
             var score = res.score;
             console.log("Got score: " + score);
-
+            updateHistory(score);
             $("#loading").hide();
             $("#score").text("Score: " + score);
         });
@@ -18,6 +55,7 @@ function getScore() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    checkHistory(2, .8)
     console.log("Getting score...");
     getScore();
 });
