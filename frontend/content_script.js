f96c1fa759dd1726bba51516fed6fb0f09e053f8
Allen Hao <allen.hao@wustl.edu>
2017-04-16 18:30:52 -0500

    Fixed echochamber with iframe boundary

--- frontend/content_script.js
+++ frontend/content_script.js
@@ -2,52 +2,59 @@ function toPercent(prop) {
     return Math.round(prop * 100, 3);
 }
 
-function checkHistory(countThreshold, propThreshold) {
-    console.log("here")
+function checkHistory(countThreshold, propThreshold, score) {
     chrome.storage.sync.get(["scoreTuple"], function(obj){
+        var percentage = ((score < 0.5) ? 1 - score : score) * 100;
         var tuple = obj["scoreTuple"];
+        var box = Boundary.findBox("#ecbanner");
+        // alert(tuple)
         var libProp = tuple[1] / tuple[0];
         var conProp = 1 - libProp;
-        console.log(tuple[0])
-        console.log(tuple[0] > countThreshold)
-        console.log(libProp)
-        console.log(libProp > propThreshold)
         if(tuple[0] > countThreshold && libProp > propThreshold) {
-            //$("#reminder").text(toPercent(libProp) + "% of your news has been liberal. Consider checking out the other side!");
+             echomsg = toPercent(libProp) + "% of your news has been liberal. Consider checking out the other side!";
         } else if(tuple[0] > countThreshold && conProp > propThreshold) {
-            //$("#reminder").text(toPercent(conProp) + "% of your news has been conservative. Consider checking out the other side!");
+             echomsg = toPercent(conProp) + "% of your news has been conservative. Consider checking out the other side!";
+        } else {
+            echomsg = null;
         }
+        if (echomsg) {
+            box.find("#loading").remove()
+            box.find("#label").text(echomsg); // color the box so it POPS
+            setTimeout(function(){
+                box.find("#loading").fadeOut()
+            	box.find("#label").text(percentage.toFixed(0) + "% chance of being " + ((score > 5) ? "liberal" : "conservative"));
+            }, 3000);
+        } else {
+            box.find("#loading").fadeOut()
+        	box.find("#label").text(percentage.toFixed(0) + "% chance of being " + ((score > 50) ? "liberal" : "conservative"));
+        }
+
     });
 }
 function updateHistory(side){
     // If new score is liberal, then increment value
-    var value = side;
+    var value = (side == "Liberal" ? 1 : 0);
     chrome.storage.sync.get(["scoreTuple"], function(obj){
         // I can change this tuple to an object if that's better
-        var tuple = obj['scoreTuple']
-        console.log(tuple)
-        tuple[0] = (tuple[0] === undefined) ? 0 : tuple[0]; // Set to 0 if no prior value
-        tuple[1] = (tuple[1] === undefined) ? 0 : tuple[1];
-        var articleCount = tuple[0] +1;
-        var liberalCount = tuple[1] + value;
-        console.log(articleCount)
-        var newTuple = [articleCount, liberalCount];
-        chrome.storage.sync.set({"scoreTuple": newTuple});
+
+        var tuple = obj['scoreTuple'] || [];
+        // tuple[0] = (tuple === undefined) ? 0 : tuple[0]; // Set to 0 if no prior value
+        tuple[0] = 1 || tuple[0]++
+        tuple[1] = value || tuple[1] + value
+        chrome.storage.sync.set({"scoreTuple": tuple});
     });
 }
 
 function fetchScore(cachedArticles) {
         var url = window.location.href;
-
         console.log("Getting score for url: " + url);
-
 	$.getJSON("https://allenhao.me/article", {
 		article: url
 	}, function(res) {
-		console.log("Got result: " + res);
+		console.log("Got result: ");
+        console.log(res);
+		updateHistory(res.side);
 
-		//updateHistory(side);
-		
 		// Scores are stored in the cache as probability of being liberal
 		var score = (res.side === "Conservative") ? 1 - res.score : res.score;
 
@@ -67,11 +74,7 @@ function fetchScore(cachedArticles) {
 }
 
 function displayScore(score) {
-	var percentage = ((score < 0.5) ? 1 - score : score) * 100;
-
-	var box = Boundary.findBox("#ecbanner");
-	box.find("#loading").fadeOut()
-	box.find("#label").text(percentage.toFixed(0) + "% chance of being " + ((score > 0.5) ? "liberal" : "conservative"));
+    checkHistory(10, .8, score);
 }
 
 function displayBanner() {
@@ -84,8 +87,8 @@ function displayBanner() {
 		var frame = $("#ecbanner").css("display", "none");
 
 		box.html("<img id='loading' src='" + loadingImageURL + "'>" +
-			"<span id='label'></span>" + 
-			"<div id='rightContainer'><a id='dashboard'>Open dashboard...</a>" + 
+			"<span id='label'></span>" +
+			"<div id='rightContainer'><a id='dashboard'>Open dashboard...</a>" +
 			"<img id='close' src='" + closeImageURL + "'></div>");
 
 		Boundary.loadBoxCSS("#ecbanner", chrome.runtime.getURL("banner_elements.css"));
@@ -103,7 +106,7 @@ function displayBanner() {
 	}
 
 	$("#ecbanner").fadeIn();
-	
+
 	// Check if we have a score for this article in the local cache
 	chrome.storage.sync.get("history", function(storage) {
 		console.log("Got storage: " + storage);
@@ -115,7 +118,8 @@ function displayBanner() {
 		var matchingArticles = cachedArticles.filter(function(article) {
 			return article.url === window.location.href;
 		});
-
+        box.find("#loading").show();
+        box.find("#label").text("Calculating score...");
 		if (matchingArticles.length === 1) {
 			console.log("Got result in cache");
 			displayScore(matchingArticles[0].score);
@@ -129,4 +133,3 @@ function displayBanner() {
 }
 
 displayBanner();
-
